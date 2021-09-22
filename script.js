aa6fecb1e9f678a9e3f0e6669f2f1d87d2a02386
Chris Hunley <cshunley21@gmail.com>
2021-09-22 14:47:44 -0500

    Initial version of exercise complete

---
+++ script.js
@@ -0,0 +1,16 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("like", "love"));
+let beenImpaled = "Oh, look at that. I've been impaled.";
+console.log(beenImpaled.slice(18, 36));
+let dotDotDot = "...";
+let skullBones = `I don't have a skull ${dotDotDot} or bones.`;
+console.log(Math.PI);
+let randomNumber = Math.random()
+randomNumber *=3
+randomNumber = Math.floor(randomNumber);
+
+let reindeers = "Reindeers are better than people."
+console.log(reindeers.split(" ").join("_"));
+console.log(Math.sqrt(2));
+let newRandomNumber = Math.random(); newRandomNumber *=7; newRandomNumber = Math.floor (newRandomNumber); newRandomNumber++;
