c19a521eb7f99e0d3a9d5e5f2052859b597a0ca2
Chris Hunley <cshunley21@gmail.com>
2021-09-23 10:18:19 -0500

    Fixed bonus

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
 console.log(warmHugs.toUpperCase());
-console.log(warmHugs.replace("like", "love"));
+warmHugs = warmHugs.replace("like", "love");
 let beenImpaled = "Oh, look at that. I've been impaled.";
 console.log(beenImpaled.slice(18, 36));
 let dotDotDot = "...";
@@ -9,8 +9,9 @@ console.log(Math.PI);
 let randomNumber = Math.random()
 randomNumber *=3
 randomNumber = Math.floor(randomNumber);
-
+randomNumber++;
+console.log(" Let It Go!".toUpperCase().repeat(2));
 let reindeers = "Reindeers are better than people."
 console.log(reindeers.split(" ").join("_"));
 console.log(Math.sqrt(2));
-let newRandomNumber = Math.random(); newRandomNumber *=7; newRandomNumber = Math.floor (newRandomNumber); newRandomNumber++;
+var newRandomNumber = Math.floor(Math.random() * (23 - 7 + 1) + 7);
